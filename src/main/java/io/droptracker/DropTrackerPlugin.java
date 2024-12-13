d9bce20cf6f89f613c90ee6fddf8abc65794ee79
joelhalen <128320003+joelhalen@users.noreply.github.com>
2024-12-13 18:26:38 -0500

    Handle more temporary world types

--- src/main/java/io/droptracker/DropTrackerPlugin.java
+++ src/main/java/io/droptracker/DropTrackerPlugin.java
@@ -49,7 +49,6 @@ import io.droptracker.api.DropTrackerApi;
 import io.droptracker.models.CustomWebhookBody;
 import io.droptracker.ui.DropTrackerPanel;
 import io.droptracker.util.ChatMessageEvent;
-import io.droptracker.util.ContainerManager;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.ChatMessage;
@@ -120,9 +119,6 @@ public class DropTrackerPlugin extends Plugin {
 	private DrawManager drawManager;
 	@Inject
 	private ChatCommandManager chatCommandManager;
-
-	@Inject
-	private ContainerManager containerManager;
 
 	/* REGEX FILTERS FOR CHAT MESSAGE DETECTION */
 	private static final Pattern COLLECTION_LOG_ITEM_REGEX = Pattern.compile("New item added to your collection log: (.*)");
@@ -366,9 +362,9 @@ public class DropTrackerPlugin extends Plugin {
 	@Subscribe
 	public void onGameTick(GameTick event) {
 		chatMessageEventHandler.onTick();
-		if (client.getGameState().equals(GameState.LOGGED_IN)) {
-			containerManager.onTick();
-		}
+//		if (client.getGameState().equals(GameState.LOGGED_IN)) {
+//			containerManager.onTick();
+//		}
 	}
 
 
@@ -396,10 +392,10 @@ public class DropTrackerPlugin extends Plugin {
 				if (sourceType != "pvp") {
 					for (ItemStack item : stack(items)) {
 						Item tempItem = new Item(item.getId(), item.getQuantity());
-						if (!containerManager.isRealDrop(tempItem)) {
-							// If the item is determined as something that was from their inv/gear, we don't send it.
-							continue;
-						}
+//						if (!containerManager.isRealDrop(tempItem)) {
+//							// If the item is determined as something that was from their inv/gear, we don't send it.
+//							continue;
+//						}
 						int itemId = item.getId();
 						int qty = item.getQuantity();
 						int price = itemManager.getItemPrice(itemId);
--- src/main/java/io/droptracker/util/ContainerManager.java
+++ src/main/java/io/droptracker/util/ContainerManager.java
@@ -1,63 +1,63 @@
-package io.droptracker.util;
-
-import io.droptracker.DropTrackerPlugin;
-import net.runelite.api.*;
-
-
-public class ContainerManager {
-    /**
-     * Helps to determine whether or not the player's newly "received" item
-     * Actually came from a loot-related event, or if it was just a result of swapping gear/looting pre-existing ground items
-     * on the same game tick that a loot-related event occurred.
-     */
-
-    private final DropTrackerPlugin plugin;
-    private final Client client;
-
-    private Item[] lastInventoryState;
-    private Item[] lastEquipmentState;
-
-    public ContainerManager(DropTrackerPlugin plugin, Client client) {
-        this.plugin = plugin;
-        this.client = client;
-    }
-
-    public void onTick() {
-        ItemContainer inventory = client.getItemContainer(InventoryID.INVENTORY);
-        ItemContainer worn = client.getItemContainer(InventoryID.EQUIPMENT);
-        
-        if (inventory != null) {
-            lastInventoryState = inventory.getItems().clone();
-        }
-        if (worn != null) {
-            lastEquipmentState = worn.getItems().clone();
-        }
-    }
-
-    public boolean isRealDrop(Item receivedItem) {
-        /** Determine if the drop is 'real' by checking the last stored inventory/worn equipment
-         * against the item. If the item was added to their containers on the same tick as a kill,
-         * we can assume it was not a real drop...?
-         * The only place I would see this potentially not working is with row(i) for coins(?)
-         * */
-        if (lastInventoryState != null) {
-            for (Item item : lastInventoryState) {
-                if (item != null && item.getId() == receivedItem.getId()) {
-                    System.out.println("Drop ignored as it was determined to be pre-existing.");
-                    return false;
-                }
-            }
-        }
-        
-        if (lastEquipmentState != null) {
-            for (Item item : lastEquipmentState) {
-                if (item != null && item.getId() == receivedItem.getId()) {
-                    System.out.println("Drop ignored as it was determined to be pre-existing.");
-                    return false;
-                }
-            }
-        }
-        
-        return true;
-    }
-}
+//package io.droptracker.util;
+//
+//import io.droptracker.DropTrackerPlugin;
+//import net.runelite.api.*;
+//
+//
+//public class ContainerManager {
+//    /**
+//     * Helps to determine whether or not the player's newly "received" item
+//     * Actually came from a loot-related event, or if it was just a result of swapping gear/looting pre-existing ground items
+//     * on the same game tick that a loot-related event occurred.
+//     */
+//
+//    private final DropTrackerPlugin plugin;
+//    private final Client client;
+//
+//    private Item[] lastInventoryState;
+//    private Item[] lastEquipmentState;
+//
+//    public ContainerManager(DropTrackerPlugin plugin, Client client) {
+//        this.plugin = plugin;
+//        this.client = client;
+//    }
+//
+//    public void onTick() {
+//        ItemContainer inventory = client.getItemContainer(InventoryID.INVENTORY);
+//        ItemContainer worn = client.getItemContainer(InventoryID.EQUIPMENT);
+//
+//        if (inventory != null) {
+//            lastInventoryState = inventory.getItems().clone();
+//        }
+//        if (worn != null) {
+//            lastEquipmentState = worn.getItems().clone();
+//        }
+//    }
+//
+//    public boolean isRealDrop(Item receivedItem) {
+//        /** Determine if the drop is 'real' by checking the last stored inventory/worn equipment
+//         * against the item. If the item was added to their containers on the same tick as a kill,
+//         * we can assume it was not a real drop...?
+//         * The only place I would see this potentially not working is with row(i) for coins(?)
+//         * */
+//        if (lastInventoryState != null) {
+//            for (Item item : lastInventoryState) {
+//                if (item != null && item.getId() == receivedItem.getId()) {
+//                    System.out.println("Drop ignored as it was determined to be pre-existing.");
+//                    return false;
+//                }
+//            }
+//        }
+//
+//        if (lastEquipmentState != null) {
+//            for (Item item : lastEquipmentState) {
+//                if (item != null && item.getId() == receivedItem.getId()) {
+//                    System.out.println("Drop ignored as it was determined to be pre-existing.");
+//                    return false;
+//                }
+//            }
+//        }
+//
+//        return true;
+//    }
+//}
