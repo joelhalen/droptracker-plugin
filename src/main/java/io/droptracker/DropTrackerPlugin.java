454c7d11ea48d25e81f644185174a5f87e934691
joelhalen <128320003+joelhalen@users.noreply.github.com>
2025-01-23 18:27:52 -0500

    Increment versioning

--- src/main/java/io/droptracker/DropTrackerPlugin.java
+++ src/main/java/io/droptracker/DropTrackerPlugin.java
@@ -177,7 +177,7 @@ public class DropTrackerPlugin extends Plugin {
 	// hopefully preventing them from being ip banned by discord
 	private int timeToRetry = 0;
 
-	public String pluginVersion = "3.3.2";
+	public String pluginVersion = "3.5";
 
 	public static final @Component int PRIVATE_CHAT_WIDGET = WidgetUtil.packComponentId(InterfaceID.PRIVATE_CHAT, 0);
 
