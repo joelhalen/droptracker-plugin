49b41e05de0b9ed04bb9b9ea160d9ab9ea382485
Koeppy <koepp.alec@gmail.com>
2025-04-06 08:10:06 -0500

    Add all PB messages, Create Game Message button, Print statement of current game message

--- src/main/java/io/droptracker/ui/DropTrackerPanel.java
+++ src/main/java/io/droptracker/ui/DropTrackerPanel.java
@@ -61,6 +61,12 @@ public class DropTrackerPanel extends PluginPanel implements DropTrackerApi.Pane
         logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
         logoPanel.add(logoLabel, BorderLayout.CENTER);
 
+        if(testing){
+            JButton testButton = new JButton("Create Game Message");
+            testButton.addActionListener(e -> plugin.chatMessageEventHandler.generateTestMessage());
+            testButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+            logoPanel.add(testButton, BorderLayout.NORTH);
+        }
         // Create the search panel
         JPanel searchPanel = new JPanel();
         searchPanel.setLayout(new GridBagLayout());
