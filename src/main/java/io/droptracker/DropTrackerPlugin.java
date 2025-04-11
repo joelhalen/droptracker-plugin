397bb4419622defdddad79c941adecf7cd2069ec
Andy <128320003+joelhalen@users.noreply.github.com>
2025-04-10 20:11:32 -0400

    Proper implementation of KCService
    Use KCService to properly store and track the source of loot/collection log events.

--- src/main/java/io/droptracker/DropTrackerPlugin.java
+++ src/main/java/io/droptracker/DropTrackerPlugin.java
@@ -6,9 +6,6 @@ BSD 2-Clause License
 
 		Copyright (c) 2022, pajlads
 
-  		Copyright (c) 2025, joelhalen
-    		https://www.droptracker.io/
-
 		Redistribution and use in source and binary forms, with or without
 		modification, are permitted provided that the following conditions are met:
 
@@ -410,6 +407,7 @@ public class DropTrackerPlugin extends Plugin {
 			case FRIENDSCHATNOTIFICATION:
 				chatMessageEventHandler.onFriendsChatNotification(chatMessage);
 		}
+		kcService.onGameMessage(chatMessage);
 	}
 
 	@Subscribe
