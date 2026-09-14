addf97781b3b0d2ec5f28fc68db708fd8c293d61
joelhalen <andy@joelhalen.net>
2026-09-14 16:02:44 +0000

    Increment versioning (v6.0.5)

--- src/main/java/io/droptracker/DropTrackerPlugin.java
+++ src/main/java/io/droptracker/DropTrackerPlugin.java
@@ -219,7 +219,7 @@ public class DropTrackerPlugin extends Plugin {
 	@Inject
 	private ClientThread clientThread;
 
-	public String pluginVersion = "6.0.4";
+	public String pluginVersion = "6.0.5";
 	// Add a new flag to track when we need to update on next available tick
 	private boolean needsPanelUpdateOnLogin = false;
 
